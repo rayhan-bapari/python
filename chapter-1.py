3b5dd936eb6f99747f18f7cacacfd41847c1afd4
rayhan-bapari <mdrayhanbapari02@gmail.com>
2025-03-25 01:02:42 +0600

    start project

---
+++ chapter-1.py
@@ -0,0 +1,16 @@
+# What is Modules?
+
+# ==> A modules is a file containing code written by somebody else that you can use in your own programs.
+
+# What is PIP?
+
+# ==> PIP is a package manager for Python packages, or modules if you like.
+
+# Type of Modules
+
+# 1. Built-in Modules
+# 2. External Modules
+
+# What REPL?
+
+# ==> REPL stands for Read-Eval-Print Loop, and it is a way you can run Python interactively.
